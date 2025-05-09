f368268617beed483c12c49258d7c12bbc85a531
Rincon <diego.rincon@wsu.edu>
2025-05-08 18:21:08 -0700

    New plot method added and checked

--- R/STBP-methods.R
+++ R/STBP-methods.R
@@ -70,3 +70,64 @@ setMethod("show", "STBP", function(object){
   cat("\nRecommendation based on provided criteria: ")
   cat(as.character(object@recommendation))
 })
+
+
+#' Draws a plot with the sequence of posterior probabilities from a sequential
+#' test of Bayesian posterior probabilities
+#'
+#' @description
+#' Method for signature \code{"STBP"} to display resulting probabilities.
+#'
+#' @param x Created as a result of a call to \code{\link{stbp_simple}} or
+#' \code{\link{stbp_composite}}.
+#' @param y Unused entry
+#'
+#' @returns
+#' A plot with the sequence of posterior probabilities
+#'
+#' @export
+#'
+#' @docType methods
+#'
+#' @examples
+#' # Testing the hypothesis of a sampled population being greater than trajectory H
+#' H <- c(2, 5, 10, 20, 40, 40, 20, 10, 5, 2)
+#'
+#' # Generating sequential samples (n = 3) from a population that is 1 below H
+#' # (H - 1)
+#'
+#' countP <- matrix(NA, 3, 10)
+#' set.seed(101)
+#' for(i in 1:10){
+#'   countP[, i] <- rpois(3, lambda = (H[i] - 1))
+#' }
+#'
+#' # Running STBP on the sample
+#'
+#' test2F <- stbp_composite(data = countP,
+#'                           greater_than = TRUE,
+#'                           hypothesis = H,
+#'                           density_func = "poisson",
+#'                           prior = 0.5,
+#'                           lower_bnd = 0,
+#'                           upper_bnd = Inf,
+#'                           lower_criterion = 0.001,
+#'                           upper_criterion = 0.999)
+#' plot(test2F)
+#'
+#' ## End (Not run)
+#' @aliases plot
+setMethod("plot", signature = c(x = "STBP", y = "missing"), function(x, y = NULL) {
+  opar <- par(no.readonly = TRUE)
+  on.exit(par(opar), add = TRUE)
+
+  par(mar = c(5, 7, 2, 2))
+  plot(x@probabilities, ylim = c(0, 1), type = "o", cex.lab = 2, yaxt = "n",
+       xaxt = "n", xlab = "Sampling bout", ylab = "", cex.axis = 2, lwd = 2)
+  axis(2, at = seq(0, 1, 0.2), cex.axis = 2, las = 2)
+  axis(1, at = seq(1, (x@iterations + 1)), labels = seq(0, x@iterations),
+       cex.axis = 2)
+  abline(h = as.numeric(as.character(x@call[9])), lty = 2)
+  abline(h = as.numeric(as.character(x@call[10])), lty = 2)
+  title(ylab = "Posterior probability", cex.lab = 2, line = 4.5)
+})
